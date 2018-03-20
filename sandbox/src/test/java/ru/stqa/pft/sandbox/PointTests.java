06e0b2dcce9fa8a0c11d975aef44d90b27571127
Ilona Zakharova <vaonli@gmail.com>
2018-03-20 09:16:21 +0300

    Home task 3. PointTests (positive and nevagative scenarios) [20.03.2018]

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,29 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+  @Test
+  public void testsDistance1 () {
+    Point p1 = new Point(4.0, 6.0);
+    Point p2 = new Point(6.0, 7.0);
+    assert p1.distance(p2) == 2.23606797749979;
+  }
+
+  @Test
+  public void testsDistance2 () {
+    Point p1 = new Point(4.0, 6.0);
+    Point p2 = new Point(6.0, 7.0);
+    Assert.assertEquals(p1.distance(p2), 2.23606797749979);
+  }
+
+  @Test
+  public void testsDistance3 () {
+    Point p1 = new Point(4.0, 6.0);
+    Point p2 = new Point(6.0, 7.0);
+    Assert.assertEquals(p1.distance(p2), 2);
+  }
+
+}
