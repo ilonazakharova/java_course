04e1bddf24c261640561089cdcf6e8c8bf20526c
Ilona Zakharova <vaonli@gmail.com>
2018-03-29 18:13:21 +0300

    29.03.2018 Задание 5

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -11,60 +11,48 @@ public class ContactHelper {
     this.wd = wd;
   }
 
-  public void returnToHomePage() { wd.findElement(By.linkText("home")).click(); }
+  public void returnToHomePage() {
+    click(By.linkText("home"));
+  }
 
-  public void submitContactCreation() { wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click(); }
+  public void submitContactCreation() {
+    click(By.name("submit"));
+  }
+
+  private void click(By locator) {
+    wd.findElement(locator).click();
+  }
 
   public void fillContactForm(ContactData contactData) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(contactData.getMiddleName());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(contactData.getNickName());
-    wd.findElement(By.name("title")).click();
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(contactData.getTitle());
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(contactData.getAddress());
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys(contactData.getHomePhone());
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(contactData.getMobilePhone());
-    wd.findElement(By.name("work")).click();
-    wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys(contactData.getWorkPhone());
-    wd.findElement(By.name("fax")).click();
-    wd.findElement(By.name("fax")).clear();
-    wd.findElement(By.name("fax")).sendKeys(contactData.getFaxPhone());
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(contactData.getEmail1());
-    wd.findElement(By.name("email2")).click();
-    wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys(contactData.getEmail2());
-    wd.findElement(By.name("email3")).click();
-    wd.findElement(By.name("email3")).clear();
-    wd.findElement(By.name("email3")).sendKeys(contactData.getEmail3());
+    fname(By.name("firstname"), contactData.getFirstName());
+    fname(By.name("middlename"), contactData.getMiddleName());
+    fname(By.name("lastname"), contactData.getLastName());
+    fname(By.name("nickname"), contactData.getNickName());
+    fname(By.name("title"), contactData.getTitle());
+    fname(By.name("company"), contactData.getCompany());
+    fname(By.name("address"), contactData.getAddress());
+    fname(By.name("home"), contactData.getHomePhone());
+    fname(By.name("mobile"), contactData.getMobilePhone());
+    fname(By.name("work"), contactData.getWorkPhone());
+    fname(By.name("fax"), contactData.getFaxPhone());
+    fname(By.name("email"), contactData.getEmail1());
+    fname(By.name("email2"), contactData.getEmail2());
+    fname(By.name("email3"), contactData.getEmail3());
 
   }
 
-  public void selectContact() { wd.findElement(By.name("selected[]")).click(); }
+  private void fname(By fnlocator, String text) {
+    click(fnlocator);
+    wd.findElement(fnlocator).clear();
+    wd.findElement(fnlocator).sendKeys(text);
+  }
+
+  public void selectContact() {
+    click(By.name("selected[]"));
+  }
 
   public void deleteSelectedContacts() {
-    wd.findElement(By.xpath("//div[@id='content']/form[2]/div[2]/input")).click();
+    click(By.xpath("//div[@id='content']/form[2]/div[2]/input"));
     wd.switchTo().alert().accept();
   }
 }
