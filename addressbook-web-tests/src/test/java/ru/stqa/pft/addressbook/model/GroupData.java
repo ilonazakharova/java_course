ca5761889c1f8277917d8a3a99000212062dbbeb
Ilona Zakharova <vaonli@gmail.com>
2018-04-01 23:35:25 +0300

    Лекция 4.7. Множества: неупорядоченные коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -1,16 +1,28 @@
 package ru.stqa.pft.addressbook.model;
 
-import java.util.Objects;
-
 public class GroupData {
+  private final String id;
   private final String name;
   private final String header;
   private final String footer;
 
   public GroupData(String name, String header, String footer) {
+    this.id = null;
     this.name = name;
     this.header = header;
     this.footer = footer;
+  }
+
+
+  public GroupData(String id, String name, String header, String footer) {
+    this.id = id;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public String getId() {
+    return id;
   }
 
   public String getName() {
@@ -28,7 +40,8 @@ public class GroupData {
   @Override
   public String toString() {
     return "GroupData{" +
-            "name='" + name + '\'' +
+            "id='" + id + '\'' +
+            ", name='" + name + '\'' +
             '}';
   }
 
@@ -36,12 +49,17 @@ public class GroupData {
   public boolean equals(Object o) {
     if (this == o) return true;
     if (o == null || getClass() != o.getClass()) return false;
+
     GroupData groupData = (GroupData) o;
-    return Objects.equals(name, groupData.name);
+
+    if (id != null ? !id.equals(groupData.id) : groupData.id != null) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
   }
 
   @Override
   public int hashCode() {
-    return 0;
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return 0; // в видео-уроке не видна эта часть
   }
 }
