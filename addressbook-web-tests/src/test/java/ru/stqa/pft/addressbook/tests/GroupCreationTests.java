dc9b1394ed4cee1ca6aced731ed3e40a077fa474
Ilona Zakharova <vaonli@gmail.com>
2018-04-03 00:24:51 +0300

    Лекция 4.9. Лямбда-выражения (анонимные функции) и элементы функционального программирования
    1. Отличия  от урока: строчка 28, вызов метода, как в уроке, не отрабатывает, метод подсвечивается серым цветом, что говорит о том, что его не используют. При запуске теста, само собой он падает.
    2. Идея предлагает решение, смотреть строчку 27 - с таким примером тест отрабатывает
    Вопрос: допустимо ли в задаче использовать метод, который предложила идея мне? Не будет ли это ошибкой?

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -13,19 +14,18 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+
+    //int max1 = after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
+    //int max1 = after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId();
+
+    group.setId(after.stream().max(Comparator.comparingInt(GroupData::getId)).get().getId()); // предлагает идея заменить, и этот тест проходит
+    //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId()); - // как в уроке, тест не отрабатывает
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
