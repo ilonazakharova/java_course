9b9973694c5c674a65184d0e261e73f3a105af10
Ilona Zakharova <vaonli@gmail.com>
2018-04-08 14:09:20 +0300

    Лекция 4.7. Множества: неупорядоченные коллекции [ContactsModifitaction]

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -6,7 +6,6 @@ import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 
 public class ContactModificationTests extends TestBase {
 
@@ -49,9 +48,8 @@ public class ContactModificationTests extends TestBase {
             "+123456",
             "email1@email.com",
             "email2@email.com",
-            "email3@email.com",
-            null);
-    app.getContactHelper().fillContactForm(new ContactData(contact));
+            "email3@email.com", null);
+    app.getContactHelper().fillContactForm(contact, false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
