a0067c8cd581d7986b2c7a322f7ec45d40b3f4d1
Ilona Zakharova <vaonli@gmail.com>
2018-04-16 23:42:19 +0300

    4.7. ContactModificationTests [16.04.2018]

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -3,10 +3,8 @@ package ru.stqa.pft.addressbook.tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
-
 import java.util.HashSet;
 import java.util.List;
-
 public class ContactModificationTests extends TestBase {
 
   @Test
