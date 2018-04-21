20597977f3e818334d3df9f375dfded7f36e94f7
Ilona Zakharova <vaonli@gmail.com>
2018-04-21 20:43:48 +0300

    Лекция 6.3. Анализ опций командой строки при запуске генератора данных [Contacts]

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,9 @@
 package ru.stqa.pft.addressbook.generators;
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,11 +13,29 @@ import java.util.List;
 
 public class ContactDataGenerator {
 
+
+  @Parameter(names = "-c", description =  "Contact count")
+  public int count;
+
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+
   public static void main (String[] agrs) throws IOException {
-    int count = Integer.parseInt(agrs[0]);
-    File file = new File(agrs[1]);
-    List<ContactData> contacts = generateContacts(count);
-    save(contacts, file);
+    ContactDataGenerator generator = new ContactDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(agrs);
+    } catch (ParameterException ex) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+  }
+
+  private void run() throws IOException {
+    List<ContactData> contacts = generateContacts(count);;
+    save(contacts, new File(file));
   }
 
   private static void save(List<ContactData> contacts, File file) throws IOException {
