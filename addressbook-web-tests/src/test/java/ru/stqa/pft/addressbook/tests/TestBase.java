c046dcf46e8ce51bb13902d50ef2f17d6a587cfa
Ilona Zakharova <vaonli@gmail.com>
2018-04-25 23:58:13 +0300

    6.10. Ошибка в TestBase

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -10,7 +10,6 @@ public class TestBase  {
   protected static final ApplicationManager app
     = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
   
-
   @BeforeSuite
   public void setUp() throws Exception { app.init(); }
 
