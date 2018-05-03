ce19ba260f5d67284c62a8d11ea37e5cdb3e2fd0
Ilona Zakharova <vaonli@gmail.com>
2018-05-03 14:52:37 +0300

    Лекция 8.6. Получение почты на встроенный в тесты почтовый сервер [ошибка в тесте]

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -19,6 +19,7 @@ public class ApplicationManager {
   private RegistrationHelper registrationHelper;
   private final Properties properties;
   private FtpHelper ftp;
+  private MailHelper mailHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -71,5 +72,12 @@ public class ApplicationManager {
       wd.get(properties.getProperty("web.baseUrl"));
     }
     return wd;
+  }
+
+  public MailHelper mail() {
+    if (mailHelper == null) {
+      mailHelper = new MailHelper(this);
+    }
+    return mailHelper;
   }
 }
