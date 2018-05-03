5c89e68c677cce0ab747cc45cc526c63992c1428
Ilona Zakharova <vaonli@gmail.com>
2018-05-03 12:27:18 +0300

    Лекция 8.5. Передача файлов на удалённую машину по протоколу FTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -18,6 +18,7 @@ public class ApplicationManager {
   private String browser;
   private RegistrationHelper registrationHelper;
   private final Properties properties;
+  private FtpHelper ftp;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -48,6 +49,13 @@ public class ApplicationManager {
       registrationHelper = new RegistrationHelper(this);
     }
     return registrationHelper;
+  }
+
+  public FtpHelper ftp() {
+    if(ftp == null) {
+      ftp = new FtpHelper(this);
+    }
+    return ftp;
   }
 
   public WebDriver getDriver() {
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -5,6 +5,9 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import java.io.File;
+import java.io.IOException;
+
 
 public class TestBase {
 
@@ -14,10 +17,15 @@ public class TestBase {
   
 
   @BeforeSuite
-  public void setUp() throws Exception { app.init(); }
+  public void setUp() throws Exception {
+    app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
+  }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() {app.stop(); }
-
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_inc.php.bak", "config_inc.php");
+    app.stop();
+  }
 }
 
