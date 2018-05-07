0950d5ccfba32f13882c8c9cf17c013ed6adb3c1
Ilona Zakharova <vaonli@gmail.com>
2018-05-07 16:00:55 +0300

    Лекция 10.1. Сервер непрерывной интеграции [Удалила тест, который выводи невалидные данные]

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -19,11 +19,6 @@ public class PointTests {
     Assert.assertEquals(p1.distance(p2), 2.23606797749979);
   }
 
-  @Test
-  public void testsDistance3 () {
-    Point p1 = new Point(4.0, 6.0);
-    Point p2 = new Point(6.0, 7.0);
-    Assert.assertEquals(p1.distance(p2), 2);
-  }
+ 
 
 }
