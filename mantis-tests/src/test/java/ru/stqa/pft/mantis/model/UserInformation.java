ed1fb7f152f55df6180b2c603f46582055468e86
Ilona Zakharova <vaonli@gmail.com>
2018-05-09 21:23:11 +0300

    Задание №18: Реализовать тест для смены пароля в MantisBT

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/model/UserInformation.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/model/UserInformation.java
@@ -1,9 +1,41 @@
 package ru.stqa.pft.mantis.model;
 
-public class UserInformation {
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "mantis_user_table")
+
+
+public class UserInformation {
+  @Id
+  @Column(name = "id")
+  private int id = Integer.MAX_VALUE;
+
+  @Column(name = "username")
   private String username;
+
+  @Column(name = "email")
   private String email;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    UserInformation that = (UserInformation) o;
+    return Objects.equals(username, that.username) &&
+            Objects.equals(email, that.email);
+  }
+
+  @Override
+  public int hashCode() {
+
+    return Objects.hash(username, email);
+  }
 
   public String getUserName() {
     return username;
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/UpdatePasswordTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/UpdatePasswordTests.java
@@ -1,9 +1,18 @@
 package ru.stqa.pft.mantis.tests;
 
 import com.sun.xml.internal.messaging.saaj.packaging.mime.MessagingException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.lanwen.verbalregex.VerbalExpression;
 import ru.stqa.pft.mantis.model.MailMessage;
+import ru.stqa.pft.mantis.model.UserInformation;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -13,29 +22,48 @@ import static org.testng.Assert.assertTrue;
 
 public class UpdatePasswordTests extends TestBase {
 
-  //@BeforeMethod //- отключаем, т.к. будем использовать отдельно стоящий сервер, а не встроенный
+  @BeforeMethod //- отключаем, т.к. будем использовать отдельно стоящий сервер, а не встроенный
   public void startMailServer() {
     app.mail().start();
+  }
+  private SessionFactory sessionFactory;
+
+
+  @BeforeClass
+  protected void initSessionFactory() throws Exception {
+    // A SessionFactory is set up once for an application!
+    final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure() // configures settings from hibernate.cfg.xml
+            .build();
+    try {
+      sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    } catch (Exception e) {
+      e.printStackTrace(); //вывод сообщения об ошибке на консоль
+      // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+      // so destroy it manually.
+      StandardServiceRegistryBuilder.destroy(registry);
+    }
   }
 
 
   @Test
   public void testUpdatePassword() throws IOException, javax.mail.MessagingException, MessagingException, SQLException {
-
-
+    Session session = sessionFactory.openSession();
+    //session.beginTransaction();
+    List<UserInformation> result = session.createQuery("from UserInformation where username like 'user%'").list();
+    UserInformation user = result.get(0);
     Long longtime = System.currentTimeMillis();
-    String user = "user1525882854734"; //логин пользователя для которого меняем пароль
+    //String user = "user1525882854734"; //логин пользователя для которого меняем пароль
     String password = "password"; //старый пароль
     String newpassword = "password1"; //новый пароль
     String email = "user1525882854734@localhost"; //емейл пользователя, которому меняют пароль
     app.navigate().login(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword")); //авторизоваться в мантис, как администратор
-    app.updateHelper().changePassword(user);
-
+    app.updateHelper().changePassword(user.getUserName());
     //List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000); //отключаем внешний почтовый сервер
     List<MailMessage> mailMessages = app.mail().waitForMail(7, 10000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.updateHelper().confirmPassword(confirmationLink, newpassword); //подтверждаем смену пароля
-    assertTrue(app.newSession().login(user, newpassword)); //проаверяем, что пользователь может авторизоваться с новым паролем
+    assertTrue(app.newSession().login(user.getUserName(), newpassword)); //проаверяем, что пользователь может авторизоваться с новым паролем
   }
 
   private String findConfirmationLink(List<MailMessage> mailMessages, String email) {
@@ -45,9 +73,8 @@ public class UpdatePasswordTests extends TestBase {
   }
 
 
-
- //@AfterMethod(alwaysRun = true) //- отключаем, т.к. будем использовать отдельно стоящий сервер, а не встроенный
-  public void stopMailServer() {
+ @AfterMethod(alwaysRun = true) //- отключаем, т.к. будем использовать отдельно стоящий сервер, а не встроенный
+ private void stopMailServer() {
     app.mail().stop();
   }
 }
